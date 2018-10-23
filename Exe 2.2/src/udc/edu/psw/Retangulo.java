f86e221c915c826b406e8f89cf067b940e3ac676
Cinthia Matte <cinthiamatte@gmail.com>
2018-10-22 22:21:39 -0300

    Retangulo Inicio

---
+++ Exe 2.2/src/udc/edu/psw/Retangulo.java
@@ -0,0 +1,22 @@
+package udc.edu.psw;
+
+public class Retangulo {
+	private int comprimento = 1;
+	private int largura = 1;
+	
+	
+	
+	
+	public int getComprimento() {
+		return comprimento;
+	}
+	public void setComprimento(int comprimento) {
+		this.comprimento = comprimento;
+	}
+	public int getLargura() {
+		return largura;
+	}
+	public void setLargura(int largura) {
+		this.largura = largura;
+	}
+}
---
+++ Exe 2.2/src/udc/edu/psw/Teste.java
@@ -0,0 +1,10 @@
+package udc.edu.psw;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
